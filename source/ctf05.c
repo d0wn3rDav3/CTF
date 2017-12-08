591e5b3803e2854f04498d91230fbaf0694caf27
d0wn3rDav3 <dforbes85@gmail.com>
2017-12-08 15:17:26 -0500

    Adding some fun...

--- source/ctf05.c
+++ source/ctf05.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<string.h>
+#include<sys/ptrace.h>
 
 char key[] = "DEAD";
 
 int welcome(void)
 {
-    printf("#Welcome to the first CTF#\n#This is REALLY easy ;-)#\n");
+    printf("#Welcome to CTF 0.5#\n#This is REALLY easy ;-)#\n");
 }
 
 int main(void)
@@ -14,6 +15,13 @@ int main(void)
     char *c = strcat(key, a);
     const char guess[24] = {};
 
+    if (ptrace(PTRACE_TRACEME, 0, 1, 0) < 0)
+    {   
+        printf("Debuggig are we?\n");
+        system("/usr/bin/firefox https://media.giphy.com/media/Vuw9m5wXviFIQ/giphy.gif");
+        return 2;
+    }
+    
     printf("Guess the password: \n");
     scanf("%s", &guess);
 
